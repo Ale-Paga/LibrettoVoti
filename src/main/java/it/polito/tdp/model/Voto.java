c9961fc5e563bc74b391653fbe0e5aea4e61d55b
Alessandro <Alessandro@LAPTOP-2Q5TD8MO.station>
2021-03-08 10:56:12 +0100

    Soluzione punto 1

---
+++ src/main/java/it/polito/tdp/model/Voto.java
@@ -0,0 +1,67 @@
+package it.polito.tdp.model;
+
+import java.time.LocalDate;
+
+
+//POJO - Plan Old Java Object
+//Java Bean
+//semplice contenitore di dati -- non ha "logica" 
+
+/**
+ * 
+ * @author Alessandro
+ *
+ */
+
+public class Voto {
+	private String nome;
+	private int voto;   //30L come lo rappresento?
+	private LocalDate data;  //data superamento esame
+	
+	
+	public Voto(String nome, int voto, LocalDate data) {
+		super();
+		this.nome = nome;
+		this.voto = voto;
+		this.data = data;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getVoto() {
+		return voto;
+	}
+
+	
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+
+	public LocalDate getData() {
+		return data;
+	}
+
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Esame " + nome + " superato con " + voto + " il " + data;
+	}
+	
+	
+
+}
