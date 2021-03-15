bf7e866fb32eb35a79ddb29b25b5b545626227e3
Alessandro <Alessandro@LAPTOP-2Q5TD8MO.station>
2021-03-15 10:21:49 +0100

    annotazione grafica

--- src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
+++ src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
@@ -15,12 +15,12 @@ public class EntryPoint extends Application {
     @Override
     public void start(Stage stage) throws Exception {
     	
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
-        Parent root = loader.load();
-        FXMLController controller = loader.getController();
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));         ////////
+        Parent root = loader.load();															////////
+        FXMLController controller = loader.getController();										////////
         
-        Libretto model = new Libretto();
-        controller.setModel(model);
+        Libretto model = new Libretto();														////////
+        controller.setModel(model);																////////
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
